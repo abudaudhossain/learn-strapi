4e4523436e2e0ec7a74656c9e9040f5453220a6a
abudaudhossain <abudaudhossainsumon@gmail.com>
2023-03-06 10:51:28 +0600

    add new plugins

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1347),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ["J9lTKAjcWzmhzinP0WGb1w==","9hfDSTNwee3CzmYOpcz1Lg==","p54XtUe4PlBRrd8p+Qcv1A=="],
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
